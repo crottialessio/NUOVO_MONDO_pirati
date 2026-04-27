216c31f35be0240c0cc5a2cfc1cbbbda62ba4dc1
ALESSANDRO MILANI <milani.alessandro.09@itisdalmine.edu.it>
2026-04-27 13:43:10 +0200

    Prima commit: creazione main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+#equipaggio: dizionario di dizionari ---> diz{cuoco:{Paga:30,Cibo:20kg,Numero:1}, ...}
+#equipaggiamento (armi cibo ecc..): dizioanrio 
+#merci(stoffa diamanti sale): dizionario di dizionari
